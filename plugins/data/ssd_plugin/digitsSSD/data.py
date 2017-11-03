10fdf8be6a9ea8e7daf477cdbaeefb3176d52ec8
wills2133 <wills2133@hotmail.com>
2017-11-02 23:28:17 -0700

    change plugin dir3

--- plugins/data/ssd_plugin/digitsSSD/data.py
+++ plugins/data/ssd_plugin/digitsSSD/data.py
@@ -166,11 +166,11 @@ class DataIngestion(DataIngestionInterface):
         ####
         print 'dataset_dir', entry
         # print 'bash /home/wills/.local/lib/python2.7/site-packages/digitsDataPluginSunnybrook/create_list.sh %s %s' % (voc_path, dataset_dir)
-        os.system( 'bash /usr/local/lib/python2.7/site-packages/digitsSSD/scripts/create_list.sh %s %s' % (self.userdata['voc_folder'], entry) )
+        os.system( 'bash /usr/local/lib/python2.7/dist-packages/digitsSSD/scripts/create_list.sh %s %s' % (self.userdata['voc_folder'], entry) )
 
         # print 'bash /home/wills/.local/lib/python2.7/site-packages/digitsDataPluginSunnybrook/create_data.sh %s %s' % (voc_path, dataset_dir)
-        os.system( 'bash /usr/local/lib/python2.7/site-packages/digitsSSD/scripts/create_data.sh %s %s' % (self.userdata['voc_folder'], entry) )
-        shutil.copy('/usr/local/lib/python2.7/site-packages/digitsSSD/scripts/labelmap_voc.prototxt', entry)
+        os.system( 'bash /usr/local/lib/python2.7/dist-packages/digitsSSD/scripts/create_data.sh %s %s' % (self.userdata['voc_folder'], entry) )
+        shutil.copy('/usr/local/lib/python2.7/dist-packages/digitsSSD/scripts/labelmap_voc.prototxt', entry)
         return feature, label
 
     @staticmethod
